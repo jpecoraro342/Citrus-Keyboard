f9e88ab82f920bbef102634dc9dc2e64ea2f908a
jpecoraro342 <jpecoraro342@ufl.edu>
2015-03-13 21:27:16 -0400

    All Joystick Input Is Retrieved

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -1,22 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ControllerInput : MonoBehaviour {
 
-	public GameObject LeftJoystick;
-	public GameObject RightJoystick;
+	public Image LeftJoystick;
+	public Image RightJoystick;
+
+	public Text DebugText;
 
 	private static string LeftTrigger = "LeftTrigger";
 	private static string RightTrigger = "RightTrigger";
 
 	private static string LeftBumper = "LeftBumper";
 	private static string RightBumper = "RightBumper";
+
+	Vector2 LeftInput;
+	Vector2 RightInput;
 	
 	void Start () {
 	
 	}
 
 	void Update () {
-		
+		float leftX = Input.GetAxisRaw("Horizontal");
+		float leftY = Input.GetAxisRaw("Vertical");
+
+		float rightX = Input.GetAxisRaw("RightHorizontal");
+		float rightY = -1*Input.GetAxisRaw("RightVertical");
+
+		LeftInput = new Vector2(leftX, leftY);
+		RightInput = new Vector2(rightX, rightY);
+
+		UpdateDebugText();
+	}
+
+	void UpdateDebugText () {
+		Quaternion leftJoystickAngle = Quaternion.Euler(LeftInput);
+		Quaternion rightJoystickAngle = Quaternion.Euler(RightInput);
+
+		DebugText.text = "Left Joystick: " + LeftInput + "\nAngle: " + leftJoystickAngle + "\n\nRight Joystick: " + RightInput + "\nAngle: " + rightJoystickAngle;
 	}
 }
