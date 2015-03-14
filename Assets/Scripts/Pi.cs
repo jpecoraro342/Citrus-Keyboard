42f9faa7d89928ed590822e713879d2f86a67216
Eric Smith <ericsmith1302@gmail.com>
2015-03-14 01:24:42 -0400

    Added makeslugs for mitch

--- Assets/Scripts/Pi.cs
+++ Assets/Scripts/Pi.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class Pi : MonoBehaviour {
 	//int radius;
 	//int numSlugs;
@@ -13,31 +14,38 @@ public class Pi : MonoBehaviour {
 		this.numSectors = 6;
 		lr = gameObject.AddComponent<LineRenderer> ();
 		drawPi ();
+		makeSlugs ();
+		//Slug s1 = new Slug (new char['A','a','?'],4,0);
+		//Slug s2 = new Slug (new char['B','b','?'],4,0);
 	}
 
 	void drawPi() {
+
 		lr.SetVertexCount (460+2*numSectors);
 		lr.SetWidth (0.1f,0.1f);
 
-		int sectorSize = 360/numSectors;
-		for (int i = 0, startAngle = 0; i < 360/sectorSize; i++, startAngle += sectorSize) {
-			drawSector ((sectorSize+2)*i,0,0,startAngle,startAngle+sectorSize);
-		}
-	}
-
-	void drawSector(int index,int centerX, int centerY, int startDegree, int endDegree) {
-		int points = endDegree - startDegree;
-		lr.SetPosition (index++, new Vector3 (centerX, centerY, 0f));
-
-		float x, y, z = 0f, angle = startDegree;
-		for (int i = 0; i < (points + 1); i++) {
+		float x, y, z = 0f, angle = 0;
+		for (int i = 0; i <= 360; i++) {
 			x = Mathf.Sin (Mathf.Deg2Rad * angle) * 4;
 			y = Mathf.Cos (Mathf.Deg2Rad * angle) * 4;
-			lr.SetPosition (index++,new Vector3(x,y,z) );
+			lr.SetPosition (i+1,new Vector3(x,y,z) );
 			angle += 1;
 		}
 
+		int sectorSize = 360/numSectors;
+		for (int i = 0, startAngle = sectorSize; i < 360/sectorSize; i++, startAngle += sectorSize) {
+			drawSector (362+(i*2),0,0,startAngle);
+		}
+	}
+
+	void drawSector(int index,int centerX, int centerY, int startDegree) {
+
 		lr.SetPosition (index++, new Vector3 (centerX, centerY, 0f));
+		float x, y, z = 0f, angle = startDegree;
+		x = Mathf.Sin (Mathf.Deg2Rad * startDegree) * 4;
+		y = Mathf.Cos (Mathf.Deg2Rad * startDegree) * 4;
+		lr.SetPosition (index++,new Vector3(x,y,z) );
+
 	}
 
 	void makeSlugs() {
