e6248f11cb99b7e1ec2b8e6add2725419fbcbced
Eric Smith <ericsmith1302@gmail.com>
2015-03-14 00:27:17 -0400

    Got sectoring to work

--- Assets/Scripts/Pi.cs
+++ Assets/Scripts/Pi.cs
@@ -21,11 +21,11 @@ public class Pi : MonoBehaviour {
 		//cylinder.transform.rotation = Quaternion.AngleAxis (90, Vector3.left);
 		//cylinder.transform.localScale = new Vector3 (3,(float) 0.1,3);
 		lr.SetVertexCount (460+2*numSectors);
-		lr.SetWidth (0.2f,0.2f);
+		lr.SetWidth (0.1f,0.1f);
 
-		int sectorSize = 30;
-		for (int i = 0, startAngle = 0; i < 360/30; i++, startAngle += sectorSize) {
-			drawSector (31*i,0,0,startAngle,startAngle+sectorSize);
+		int sectorSize = 360/numSectors;
+		for (int i = 0, startAngle = 0; i < 360/sectorSize; i++, startAngle += sectorSize) {
+			drawSector ((sectorSize+2)*i,0,0,startAngle,startAngle+sectorSize);
 		}
 	}
 
@@ -34,7 +34,7 @@ public class Pi : MonoBehaviour {
 		lr.SetPosition (index++, new Vector3 (centerX, centerY, 0f));
 
 		float x, y, z = 0f, angle = startDegree;
-		for (int i = 0; i < points; i++) {
+		for (int i = 0; i < (points + 1); i++) {
 			x = Mathf.Sin (Mathf.Deg2Rad * angle) * 4;
 			y = Mathf.Cos (Mathf.Deg2Rad * angle) * 4;
 			lr.SetPosition (index++,new Vector3(x,y,z) );
