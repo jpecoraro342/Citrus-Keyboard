1751f1a48b0f30119d2cca2da9adb35f95093b09
Eric Smith <ericsmith1302@gmail.com>
2015-03-13 20:49:30 -0400

    Added Pi file

---
+++ Assets/Pi.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pi : MonoBehaviour {
+	int radius;
+	// Use this for initialization
+	void Start (int radius) {
+		this.radius = radius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
