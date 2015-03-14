d620fef780c23d2b6782dc28bc1a7f93c4b40a94
jpecoraro342 <jpecoraro342@ufl.edu>
2015-03-13 20:48:01 -0400

    Put stuff in folders so they actually show up in git. Ignore the controller specific stuff

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerInput : MonoBehaviour {
+
+	public GameObject LeftJoystick;
+	public GameObject RightJoystick;
+
+	private static string LeftTrigger = "LeftTrigger";
+	private static string RightTrigger = "RightTrigger";
+
+	private static string LeftBumper = "LeftBumper";
+	private static string RightBumper = "RightBumper";
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		
+	}
+}
