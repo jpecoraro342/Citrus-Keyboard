1d4a93174d9fc65bdb087d5ecb514112ff3aced3
jpecoraro342 <jpecoraro342@ufl.edu>
2015-03-13 23:42:31 -0400

    Joystick now knows when to "Select" and "Cancel"

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -6,6 +6,8 @@ public class ControllerInput : MonoBehaviour {
 	public float VisualJoystickMagnitude = 30;
 	public float JoystickSmoothing = 20;
 
+	public float CenteredThreshold = .1f;
+
 	public Image LeftJoystick;
 	public Image RightJoystick;
 
@@ -19,6 +21,9 @@ public class ControllerInput : MonoBehaviour {
 
 	Vector2 LeftInput;
 	Vector2 RightInput;
+
+	bool LeftJoystickCentered;
+	bool RightJoystickCentered;
 	
 	void Start() {
 	
@@ -34,6 +39,7 @@ public class ControllerInput : MonoBehaviour {
 		LeftInput = new Vector2(leftX, leftY);
 		RightInput = new Vector2(rightX, rightY);
 
+		CheckJoystickSelections();
 		UpdateJoystickPositions();
 		UpdateDebugText();
 	}
@@ -46,6 +52,58 @@ public class ControllerInput : MonoBehaviour {
 		RightJoystick.rectTransform.anchoredPosition = Vector2.Lerp(RightJoystick.rectTransform.anchoredPosition, RightPosition, JoystickSmoothing * Time.deltaTime);
 	}
 
+
+	//Check to See if the Left Joystick selected "Cancel" and the Right Joystick "Selected the Character"
+	void CheckJoystickSelections() {
+		bool CancelSelected = DidLeftJoystickCenter();
+		bool CharacterSelected = DidRightJoystickCenter();
+
+		if (CancelSelected) {
+			//TODO: Tell the manager that a character was selected
+		}
+
+		if (CharacterSelected) {
+			//TODO: Tell the manager that a character was selected
+		}
+	}
+
+	bool DidLeftJoystickCenter() {
+		float VectorMagnitude = LeftInput.magnitude;
+
+		if (VectorMagnitude > CenteredThreshold) {
+			//Joystick is not centered
+			LeftJoystickCentered = false;
+			return false;
+		}
+		else if (LeftJoystickCentered) {
+			//Joystick is Centered, and Was Previously
+			return false;
+		}
+		else {
+			LeftJoystickCentered = true;
+			return true;
+		}
+	}
+
+	bool DidRightJoystickCenter() {
+		float VectorMagnitude = RightInput.magnitude;
+		
+		if (VectorMagnitude > CenteredThreshold) {
+			//Joystick is not centered
+			RightJoystickCentered = false;
+			return false;
+		}
+		else if (RightJoystickCentered) {
+			//Joystick is Centered, and Was Previously
+			return false;
+		}
+		else {
+			RightJoystickCentered = true;
+			return true;
+		}
+	}
+
+	//Debugging
 	void UpdateDebugText() {
 		float leftJoystickAngle = getVectorAngle(LeftInput);
 		float rightJoystickAngle = getVectorAngle(RightInput);
@@ -53,6 +111,7 @@ public class ControllerInput : MonoBehaviour {
 		DebugText.text = "Left Joystick: " + LeftInput + "\nAngle: " + leftJoystickAngle + "\n\nRight Joystick: " + RightInput + "\nAngle: " + rightJoystickAngle;
 	}
 
+	//Math Methods
 	float getVectorAngle(Vector2 InputVector) {
 		if (InputVector.x == 0 && InputVector.y == 0) {
 			return 0;
